d5f78c880697dac8fffe2179bf6e0a8ba81f6454
thoughtsunificator <oss@thoughtsunificator.me>
2023-05-28 14:45:49 +0000

    Update gh-pages to output generated at 0332d18

---
+++ precache-manifest.b044eb7b673edc6f145071e33b919155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3d8eb64b64d7dbec27ccff8ba1ee98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6154c2bf3614df8c28e7",
+    "url": "/static/css/main.17b8bafe.chunk.css"
+  },
+  {
+    "revision": "13f445be7b03d795ed30",
+    "url": "/static/js/2.7d3f8454.chunk.js"
+  },
+  {
+    "revision": "5c9dafe27ad52b349956396cd7392032",
+    "url": "/static/js/2.7d3f8454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6154c2bf3614df8c28e7",
+    "url": "/static/js/main.bd6308ea.chunk.js"
+  },
+  {
+    "revision": "fe276d11f25d887bf084",
+    "url": "/static/js/runtime-main.4b6c5236.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.62d6ddaa6bc283d3f0d74286648fe5c5.js"
+  "/precache-manifest.b044eb7b673edc6f145071e33b919155.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
